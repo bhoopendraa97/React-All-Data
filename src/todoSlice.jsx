4ff3de27ca14573839b9651aef2f511028cc44d1
bhoopendraa97 <bhoopendraa77@gmail.com>
2024-12-23 20:53:20 +0530

    todo

---
+++ src/todoSlice.jsx
@@ -0,0 +1,59 @@
+
+/*  import { createSlice } from "@reduxjs/toolkit";
+const todoSlice=createSlice({
+
+     name: "todo",
+     initialState:{
+         task:[]
+     },
+     reducers:{
+      addTask:(state,actions)=>{
+       state.task.push(actions.payload);
+      },
+      deleteTask:(state,actions)=>{
+        console.log(actions.payload.id);
+        state.task=state.task.filter(key=>key.id!=actions.payload.id);
+      },
+      editDataSave:(state,actions)=>{
+        for (var i=0; i<state.task.length; i++){
+            if (state.task[i].id==actions.payload.id){
+                state.task[i].work=actions.payload.work;
+            }
+        }
+      }
+        
+    } 
+
+})
+export const {addTask,deleteTask,editDataSave}=todoSlice.actions;
+export default todoSlice.reducer;   */
+
+import { createSlice } from "@reduxjs/toolkit";
+const todoSlice=createSlice({
+       name:"todo",
+       initialState:{
+            task:[]
+       },
+       reducers:{
+       addTask:(state,actions)=>{
+          state.task.push(actions.payload);
+       },
+       deleteTask:(state,action)=>{
+       console.log(action.payload.id);
+       state.task=state.task.filter(key=>key.id!=action.payload.id)
+       },
+       editDataSave:(state,action)=>{
+          for( var i=0; i<state.task.length;i++){
+               if(state.task[i].id==actions.payload.id){
+                    state.task[i].work=action.payload.work
+               }
+          }
+       }
+       }
+})
+
+
+
+
+
+
